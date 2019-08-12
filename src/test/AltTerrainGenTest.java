a7a6530a59e894e32c8edb110f1df3ef0caf58dc
Morgan Allen <>
2019-08-12 22:44:25 +0100

    More refinements for updated terrain-generation.

--- src/test/AltTerrainGenTest.java
+++ src/test/AltTerrainGenTest.java
@@ -10,13 +10,13 @@ import java.awt.Color;
 public class AltTerrainGenTest implements TileConstants {
 
   
-  final static int colours[] = {
-    new Color(0 , 0 , 1f).getRGB(),
-    new Color(0 , 1f, 0 ).getRGB(),
-    new Color(1f, 1f, 0 ).getRGB(),
-    new Color(0.5f, 0.5f, 0.5f).getRGB()
-  };
-  final static int PATCH = 16, ROUND = 8;
+  final static int
+    IND_WATER = 0,
+    IND_SHORE = 1,
+    IND_LAND  = 2,
+    IND_TREES = 3,
+    IND_ROCKS = 4
+  ;
   
   
   int size, res;
@@ -40,10 +40,10 @@ public class AltTerrainGenTest implements TileConstants {
   }
   
   
-  void doTerrainGenTwo() {
+  void doTerrainGen(int mapSize, int seedSize, int gradient[]) {
 
-    size = 256;
-    res = (size / PATCH) + 1;
+    size = mapSize;
+    res = seedSize + 1;
     
     int regionsWanted = (res * res) / (4 * 4);
     int lakeAreaWanted = (res * res) / 4;
@@ -56,6 +56,7 @@ public class AltTerrainGenTest implements TileConstants {
     
     class Region extends Box2D {
       boolean waterRegion = false;
+      float sizeRating = 0;
     };
 
     Region boxGrid[][] = new Region[res][res];
@@ -72,18 +73,18 @@ public class AltTerrainGenTest implements TileConstants {
       final Region parent = working.removeFirst();
       
       float splitChance = 1f - (regions.size() * 1f / regionsWanted);
-      splitChance += parent.maxSide() / res;
+      splitChance += parent.maxSide() / (res - 2);
       splitChance /= 2;
       if (parent.maxSide() < 4) splitChance = 0;
+      boolean doSplit = Rand.num() <= splitChance;
       
-      float lakeChance = 1 - (parent.maxSide() / (res - 2));
-      lakeChance -= lakesArea * 1f / lakeAreaWanted;
+      if (display) {
+        I.say("Parent region: "+parent+", splitting: "+doSplit);
+      }
       
-      
-      if (Rand.num() > splitChance) {
+      if (! doSplit) {
         regions.add(parent);
         for (Coord c : Visit.grid(parent)) boxGrid[c.x][c.y] = parent;
-        parent.waterRegion = Rand.num() < lakeChance;
         if (parent.waterRegion) lakesArea += parent.area();
       }
       else {
@@ -118,18 +119,46 @@ public class AltTerrainGenTest implements TileConstants {
         }
       }
       
+      if (working.empty()) {
+        List <Region> regionSort = new List <Region> () {
+          protected float queuePriority(Region r) {
+            return r.sizeRating;
+          }
+        };
+        for (Region r : regions) {
+          r.sizeRating = Rand.num();
+          regionSort.add(r);
+        }
+        regionSort.queueSort();
+        
+        for (Region r : regionSort) {
+          float area = (r.xdim() + 1) * (r.ydim() + 1);
+          if ((lakesArea + (area / 2)) >= lakeAreaWanted) break;
+          
+          r.waterRegion = true;
+          lakesArea += area;
+          
+          if (display) {
+            I.say("Lakes area: "+lakesArea+"/"+lakeAreaWanted);
+          }
+        }
+      }
+      
       if (display) {
         for (Coord c : Visit.grid(0, 0, res, res, 1)) {
-          typeDisplay[c.x][c.y] = colours[1];
+          Region r = boxGrid[c.x][c.y];
+          int tone = gradient[IND_LAND];
+          if (r != null && r.waterRegion) tone = gradient[IND_WATER];
+          typeDisplay[c.x][c.y] = tone;
         }
         for (Region r : working) {
           for (Coord c : Visit.perimeter(r)) {
-            typeDisplay[c.x][c.y] = colours[3];
+            typeDisplay[c.x][c.y] = gradient[IND_ROCKS];
           }
         }
         for (Region r : regions) {
           for (Coord c : Visit.perimeter(r)) {
-            typeDisplay[c.x][c.y] = colours[3];
+            typeDisplay[c.x][c.y] = gradient[IND_ROCKS];
           }
         }
         I.present("Area Borders", res * 10, res * 10, typeDisplay);
@@ -145,14 +174,14 @@ public class AltTerrainGenTest implements TileConstants {
     
     for (Coord c : Visit.grid(0, 0, res, res, 1)) {
       Region r = boxGrid[c.x][c.y];
-      type[c.x][c.y] = (r != null && r.waterRegion) ? 0 : 1;
+      type[c.x][c.y] = (r != null && r.waterRegion) ? IND_WATER : IND_LAND;
     }
     for (Region r : regions) {
       for (Coord c : Visit.perimeter(r)) {
         //
         //  We ensure that the absolute edges of the map are always filled-
         boolean border = c.x == 0 || c.x == max || c.y == 0 || c.y == max;
-        int borderType = r.waterRegion ? 1 : 3;
+        int borderType = r.waterRegion ? IND_WATER : IND_ROCKS;
         //
         //  This is needed to ensure borders aren't visited twice when shared
         //  by multiple regions-
@@ -164,7 +193,7 @@ public class AltTerrainGenTest implements TileConstants {
         //  leave it be-
         int scatter = Rand.index(12);
         if (scatter < 3) {
-          if (r.waterRegion) setVal(type, c.x, c.y, 0);
+          if (r.waterRegion) setVal(type, c.x, c.y, IND_LAND);
           continue;
         }
         int dir = Nums.clamp(scatter - 3, 5) * 2;
@@ -176,7 +205,7 @@ public class AltTerrainGenTest implements TileConstants {
     
     if (display) {
       for (Coord c : Visit.grid(0, 0, res, res, 1)) {
-        typeDisplay[c.x][c.y] = colours[type[c.x][c.y]];
+        typeDisplay[c.x][c.y] = gradient[type[c.x][c.y]];
       }
       I.present("Scattered Borders", res * 10, res * 10, typeDisplay);
     }
@@ -184,8 +213,8 @@ public class AltTerrainGenTest implements TileConstants {
     for (Coord c : Visit.grid(0, 0, res, res, 1)) {
       int typeAt = type[c.x][c.y];
       float high = 0.5f;
-      if (typeAt == 0) high = 0;
-      if (typeAt == 3) high = 1.0f;
+      if (typeAt == IND_WATER) high = 0;
+      if (typeAt == IND_ROCKS) high = 1.0f;
       heightSeed[c.x][c.y] = high;
     }
     
@@ -193,39 +222,73 @@ public class AltTerrainGenTest implements TileConstants {
     HeightMap mapGrid = new HeightMap(size + 1, heightSeed, 1, 0.5f);
     HeightMap fertile = new HeightMap(size + 1);
     
-    final float waterHigh = 0.3f, rocksHigh = 0.7f, treesFert = 0.33f;
+    final float waterHigh = 0.33f, rocksHigh = 0.66f;
+    final float treesFert = 0.40f, shoreHigh = 0.38f;
     
     for (Coord c : Visit.grid(0, 0, size, size, 1)) {
       float high = mapGrid.value()[c.x][c.y];
+      float waterDist = (high - waterHigh) * (1 - waterHigh) * 0.5f;
       
       float fert = fertile.value()[c.x][c.y];
       fert = (fert - 0.25f) * 2;
       fert = Nums.min(fert, 0.95f - high);
-      boolean tree = fert < treesFert && Rand.index(4) > 0;
-      if (fert > treesFert && Rand.index(16) == 0) tree = true;
+      boolean tree = fert < treesFert && (Rand.index(4) > 2 || high > 0.61f);
+      if (fert > treesFert && Rand.num() < waterDist) tree = true;
       
-      int index = tree ? 2 : 1;
-      if (high < waterHigh) index = 0;
-      if (high > rocksHigh) index = 3;
-      grid[c.x][c.y] = colours[index];
+      int index = tree ? IND_TREES : IND_LAND;
+      if (high < shoreHigh) index = IND_SHORE;
+      if (high < waterHigh) index = IND_WATER;
+      if (high > rocksHigh) index = IND_ROCKS;
+      
+      grid[c.x][c.y] = gradient[index];
     }
     
     if (display) {
-      I.present("Alt Terrain", size * 2, size * 2, grid);
+      I.present("Alt Terrain", 512, 512, grid);
     }
   }
   
+  
+  final static int colours_tropic[] = {
+    new Color(0.0f, 0.0f, 0.7f).getRGB(),
+    new Color(0.8f, 0.8f, 0.7f).getRGB(),
+    new Color(0.3f, 0.5f, 0.1f).getRGB(),
+    new Color(0.0f, 0.4f, 0.0f).getRGB(),
+    new Color(0.2f, 0.3f, 0.3f).getRGB()
+  };
+  
+  final static int colours_tundra[] = {
+    new Color(0.0f, 0.2f, 0.5f).getRGB(),
+    new Color(0.7f, 0.7f, 0.8f).getRGB(),
+    new Color(0.5f, 0.5f, 0.6f).getRGB(),
+    new Color(0.0f, 0.2f, 0.1f).getRGB(),
+    new Color(0.7f, 0.7f, 0.8f).getRGB()
+  };
+  
+  final static int colours_desert[] = {
+    new Color(0.0f, 0.2f, 0.5f).getRGB(),
+    new Color(0.5f, 0.5f, 0.2f).getRGB(),
+    new Color(0.5f, 0.4f, 0.2f).getRGB(),
+    new Color(0.0f, 0.3f, 0.1f).getRGB(),
+    new Color(0.3f, 0.3f, 0.3f).getRGB()
+  };
+  
+  final static int colours_wastes[] = {
+    new Color(0.3f, 0.3f, 0.4f).getRGB(),
+    new Color(0.5f, 0.5f, 0.2f).getRGB(),
+    new Color(0.3f, 0.3f, 0.3f).getRGB(),
+    new Color(0.4f, 0.3f, 0.1f).getRGB(),
+    new Color(0.2f, 0.2f, 0.2f).getRGB()
+  };
   
   
   public static void main(String args[]) {
     AltTerrainGenTest t = new AltTerrainGenTest();
-    t.doTerrainGenTwo();
+    t.doTerrainGen(256, 16, colours_tropic);
   }
   
   
 }
-
-
 
 
 
